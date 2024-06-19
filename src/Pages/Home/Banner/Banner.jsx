228d26840f67890d3d5d27eb395b4ee291684b80
PRONOY <pronoybanik82@gmail.com>
2024-06-19 22:03:51 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,37 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import imageOne from "../../../assets/home/01.jpg";
+import imageTwo from "../../../assets/home/02.jpg";
+import imageThree from "../../../assets/home/03.png";
+import imageFore from "../../../assets/home/04.jpg";
+import imageFive from "../../../assets/home/05.png";
+import imageSix from "../../../assets/home/05.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <Carousel>
+        <div>
+          <img src={imageOne} />
+        </div>
+        <div>
+          <img src={imageTwo} />
+        </div>
+        <div>
+          <img src={imageThree} />
+        </div>
+        <div>
+          <img src={imageFore} />
+        </div>
+        <div>
+          <img src={imageFive} />
+        </div>
+        <div>
+          <img src={imageSix} />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,54 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import slider1 from "../../../assets/home/slide1.jpg";
+import slider2 from "../../../assets/home/slide2.jpg";
+import slider3 from "../../../assets/home/slide3.jpg";
+import slider4 from "../../../assets/home/slide4.jpg";
+import slider5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="my-8"
+      >
+        <SwiperSlide>
+          <img src={slider1} alt="" />
+          <p className="text-2xl text-center -mt-12 text-black font-bold uppercase">
+            Salads
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider2} alt="" />
+          <p className="text-2xl text-center -mt-12 text-black font-bold uppercase">
+            Pizzas
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider3} alt="" />
+          <p className="text-2xl text-center -mt-12 text-black font-bold uppercase">
+            Soups
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider4} alt="" />
+          <p className="text-2xl text-center -mt-12 text-black font-bold uppercase">
+            desert
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider5} alt="" />
+          <p className="text-2xl text-center -mt-12 text-black font-bold uppercase">
+            Salads
+          </p>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
